96331b1e646bab143b9d83ab4125bd45152eb6a2
Deibson <deibsoncarvalho@gmail.com>
2021-02-26 11:03:08 -0300

    Include support to 404.html

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Small static web server for testing web pages.
 
+written in Node, the script only provides files in html, js, css and simple images.
+
+I repeat, it is very simple, it does not provide the necessary structure for an automated test, staging or production web server. 
+
+**Nowhere use this script in production! There are no security, stability or performance policies for a real server.**
+
+## Install
+
+Download the zipped folder, extract and at the root, run the command:
+
+~~~
+npm install
+~~~
+
+## Use
+
+To use the server, after installation, run the command:
+
+~~~
+npm start
+~~~
+
--- server.js
+++ server.js
@@ -15,38 +15,54 @@ const webserver = http.createServer();
 
 webserver.on('request', (req, res) => {
 
-    let fileName = Url(req.url).pathname
-
+    let fileName = Url(req.url).pathname;
+    
+    console.log(`URL: ${req.url}`);
+    
     if(fileName === '/') {
         fileName = defaultIndex;
     } else if (!(fileName.split('/').pop().indexOf('.') > -1)) {
         fileName = path.join(fileName, defaultIndex);
-    }
+    } 
 
-    const fullPath = path.join(rootFolder, fileName);
+    let fullPath = path.join(rootFolder, fileName);
     const extension = fileName.substr(fileName.lastIndexOf('.') + 1);
 
     console.log(`request: ${fileName}`)
     
     fs.readFile(fullPath, (err, data) => {
         if(err) {
-            res.writeHead(404);
-            res.end();
             console.log(`Not found: ${fullPath}`)
+
+            fullPath = path.join(rootFolder, '404.html');
+
+            fs.readFile(fullPath, (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.end();
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html',
+                        'Content-Length': data.length
+                       });
+                    res.end(data);        
+                }
+            });
+                        
         } else {
             res.writeHead(200, {
                                 'Content-Type': types[extension] || 'text/plain',
                                 'Content-Length': data.length
                                });
             res.end(data);
-            console.log(`found: ${fullPath}`)
+            console.log(`found: ${fullPath}`);
         }
     });
 
 });
 
 // rename sys process
-process.title = 'MyWebServer';
+process.title = 'LiteWebServer';
 
 // start listen de web server process
 webserver.listen(port, () => {
